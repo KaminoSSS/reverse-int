d4e7155ca7bc0bd2e5c0fbd4e486644b6d381be9
Egor Perfiliev <perfilev.egor2016@gmail.com>
2023-01-09 21:33:05 +0300

    feat: implement reverse int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+	let str = n + '';
+	let result = '';
+	for(let i = str.length - 1; i >= 0; i--){
+		if (str[i] == '-'){
+			continue ;
+		} else{
+			result += str[i];
+		}
+	}
+	return result
 }
